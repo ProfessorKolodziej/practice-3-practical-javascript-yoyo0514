22dfcc7b49b4e922299d31781e74f731cea23718
yoyo0514 <157999122+yoyo0514@users.noreply.github.com>
2024-04-10 15:30:37 -0400

    1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,27 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "yoyo";
+const myAge = 100;
+
+console.log(myAge);
+
+// show and hide an element on the page
+
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+//Write a function that toggoles the show.hide classes
+function toggleParagraph() {
+    toggledParagraph.classList.toggle("hide");
+    toggledParagraph.classList.toggle("show");
+    console.log("My toggle function");
+    console.log(toggledParagraph);
+}
+
+
+// Find the button and toggle the paragraph when we click on it
+const toggleButton = document. querySelector("#toggle-control");
+//console.log(toggleButton);
+toggleButton.addEventListener("click", toggleParagraph);
